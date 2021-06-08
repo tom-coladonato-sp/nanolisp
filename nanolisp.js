70b01ac9d320fa36c974a0dcf55a10ead512bd73
Tom Coladonato <tom.coladonato@sailpoint.com>
2021-06-08 11:57:23 -0500

    fix [] falsyness

--- nanolisp.js
+++ nanolisp.js
@@ -35,7 +35,46 @@ export const atom = token => {
     }
 }
 
-export const standardEnv = (() => {
+class Env { // a ChainMap
+    constructor(params, args, outer = builtins) {
+        if(params === null && args === null) { // root env
+            this.map = outer;
+        } else {
+            if(params.length !== args.length) {
+                throw new SyntaxError("mismatched params and args: p=" + params + ", a=" + args);
+            }
+            this.map = {};
+            for (let i = 0; i < args.length; i++) {
+                this.map[params[i]] = args[[i]];
+            }
+            this.outer = outer;
+        }
+
+    }
+    get(key) {
+        return key in this.map
+            ? this.map[key]
+            : this.outer !== undefined
+                ? this.outer.get(key)
+                : null;
+    }
+    put(key, value) {
+        this.map[key] = value;
+    }
+}
+
+class Proc {
+    constructor(params, body, env) {
+        this.params = params;
+        this.body = body;
+        this.env = env;
+    }
+    call(...args) {
+        return nanoEval(this.body, new Env(this.params, args, this.env));
+    }
+}
+
+export const builtins = (() => {
     const env = {};
 
     // I am lazy
@@ -60,9 +99,15 @@ export const standardEnv = (() => {
     };
     env['apply'] = (a, b) => a(b);
     env['begin'] = (a, b) => b;
-    env['car'] = (a) => a;
-    env['cdr'] = (a, ...b) => b;
+    env['car'] = (a) => {
+        return a[0];
+    };
+    env['cdr'] = (a) => {
+        let [first, ...rest] = a;
+        return rest;
+    };
     env['cons'] = (a, b) => [...a, ...b];
+    env['equal?'] = (a, b) => a === b;
     env['length'] = (a) => a.length;
     env['list'] = (...a) => [...a];
     env['list?'] = (a) => Array.isArray(a);
@@ -74,30 +119,60 @@ export const standardEnv = (() => {
     env['symbol?'] = a => a instanceof String;
     env['globals'] = () => Object.keys(env);
 
-    return env;
+    return new Env(null, null, env);
 })();
 
-export const nanoEval = (x, env=standardEnv) => {
-    if(typeof x === 'string') {
-        return env[x];
+export const nanoEval = (x, env= builtins) => {
+    console.log(JSON.stringify(x));
+    if(env.outer !== undefined) {
+        console.log(env.map);
+    } else {
+        console.log("ROOT");
     }
-    if(!isNaN(x)) {
+    console.log("=====");
+    if(typeof x === 'string') {
+        if (x[0] === '"' && x[x.length - 1] === '"') {
+            return x;
+        } else {
+            return env.get(x);
+        }
+    } else if (!Array.isArray(x)) {
         return x;
     }
-    if(x[0] === 'if') {
-        let [_, test, conseq, alt] = x;
-        let exp = nanoEval(test) ? conseq : alt;
+
+    let [op, ...args] = x;
+    if (op === 'quote') {
+        return args[0];
+    } else if (op === 'if') {
+        let [test, conseq, alt] = args;
+        let testResult = nanoEval(test, env);
+        if(Array.isArray(testResult) && testResult.length  === 0) { // empty list is falsy in lisp
+            testResult = false;
+        }
+        let exp = testResult ? conseq : alt;
         return nanoEval(exp, env);
+    } else if (op === 'define') {
+        let [symbol, exp] = args;
+        env.put(symbol, nanoEval(exp, env));
+    } else if (op === 'set!') {
+        let [symbol, exp] = args;
+        env.get(symbol).put(symbol, nanoEval(exp, env));
+    } else if (op === 'lambda') {
+        let [params, body] = args;
+        return new Proc(params, body, env);
+    } else {
+        let proc = nanoEval(op, env);
+        let vals = args.map(arg => nanoEval(arg, env));
+        if(proc instanceof Proc) {
+            return proc.call(...vals);
+        } else {
+            try {
+                return proc(...vals);
+            } catch (e) {
+                debugger;
+            }
+        }
     }
-    if(x[0] === 'define') {
-        let [_, symbol, exp] = x;
-        env[symbol] = nanoEval(exp, env);
-        return undefined;
-    }
-    let proc = nanoEval(x[0], env);
-    let [head, ...tail] = x;
-    let args = tail.map(arg => nanoEval(arg, env));
-    return proc(...args);
 }
 
 export const repl = async (prompt='nanolisp.js>') => {
@@ -116,7 +191,6 @@ export const repl = async (prompt='nanolisp.js>') => {
                     console.error(e);
                 }
                 resolve();
-
             });
         });
     }
